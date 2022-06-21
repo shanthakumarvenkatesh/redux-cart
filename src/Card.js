1a2f8491f7390e6e8535fa725c02fff64feb99f2
shanthakumarvenkatesh <shanthakumarvenkatesh>
2022-06-21 13:49:48 +0530

    first commit

---
+++ src/Card.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useState } from 'react';
+import {connect} from 'react-redux';
+import {AddProduct} from './actions';
+import { RemoveProduct } from './actions';
+
+const Card = ({products,AddProduct,RemoveProduct}) => {
+    const [item,setItem] = useState("");
+    const submitHandler = async e =>{
+        e.preventDefault();
+        if(item !== ""){
+            await AddProduct({name:item});
+            setItem("");
+        }
+        setItem("");
+    }
+    return (
+        <center>
+            <div class="card" style={{"width": "18rem"}}>
+                <div class="card-body">
+                    <form onSubmit={submitHandler}>
+                    <input type="text" placeholder="Add Products" value={item} onChange={e => setItem(e.target.value)}/> <br />
+
+                        <button type="submit" className="btn btn-success" style={{"position":"relative","top":"20px"}} >
+                        Add Product 
+                        </button> 
+                    </form>
+                    <br />
+                    {products.map(product => {
+                        return(
+                            <div className="item" style={{"paddingTop":"10px"}} >{product.name} 
+                            <span onClick={()=> RemoveProduct(product.name)} class="badge square-pill bg-danger" style={{"float": "right","padding":"6px 10px 6px 10px"}}>
+                            X</span></div>
+                        )
+                    })}
+                </div>
+            </div>
+        </center>
+    )
+}
+
+const mapStateToProps = (state) =>({
+    products: state
+})
+
+export default connect(mapStateToProps,{AddProduct,RemoveProduct})(Card);
